29b7c22d33ed88c85ba881203206c218292ad6a0
HWANG-DAYE <sindy1345@gmail.com>
2025-06-05 09:50:51 +0900

    250604 JSP수업

--- project0526/test.sql
+++
@@ -1,4 +0,0 @@
-select * from register;
-insert into register values('asdf', '1234', '홍길동', 'asdf@naver.com');
-delete from register where id = 'asdf';
-update register set pwd = ?, name = ?, email = ? where id = ?;
--- project0527/test.sql
+++
@@ -1,6 +0,0 @@
-select * from board;
-insert into board(title, content, writer) values('제목1', '내용1', 'asdf');
-update board set title = '변경', content = '변경' where num = 1 and writer = 'asdf';
-delete from board where num=1;
-
-select * from member;
